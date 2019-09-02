0caad4b4e93bd2b960efdd856282cc06c8a38b08
Dima Skibin <sixtynice77@gmail.com>
2019-09-02 20:23:32 +0300

    Make demo About Page

---
+++ src/components/About/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from '../Header/Header';
+import SvgProgrammer from '../SvgProgrammer/SvgProgrammer';
+import './About.css';
+
+export default function About() {
+    return (
+        <section className="about">
+        <Header/>
+        <div className="about-content">
+        <SvgProgrammer/>
+        <p className="about-text"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque facere dicta non neque maxime incidunt saepe dignissimos repellat praesentium? Debitis cupiditate fuga voluptatum recusandae, aspernatur itaque iste impedit vero est. Lorem, ipsum dolor sit amet r itaque iste impedit vero est. Ernatur itaque iste impedit vero est. Lorem, ipsum dolor sit amet r itaque iste impedit vero est.</p>
+        </div>
+        </section>
+    )
+}
+
+
+
+
