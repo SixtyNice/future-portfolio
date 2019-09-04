82309745a029321efa4a99574b35c2a07debbd87
Dima Skibin <sixtynice77@gmail.com>
2019-09-04 17:57:25 +0300

    Make height 100vh

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,7 +7,6 @@ import Button from '../Button/Button';
 export default function About() {
     return (
         <section className="about">
-        <Header/>
         <div className="about-content">
         <SvgProgrammer/>
         <p className="about-text"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque facere dicta non neque maxime incidunt saepe dignissimos repellat praesentium? Debitis cupiditate fuga voluptatum recusandae, aspernatur itaque iste impedit vero est. Lorem, ipsum dolor sit amet r itaque iste impedit vero est. Ernatur itaque iste impedit vero est. Lorem, ipsum dolor sit amet r itaque iste impedit vero est.</p>
