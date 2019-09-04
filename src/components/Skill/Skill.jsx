07c175d8cb644192c2e016f40e8c5282d1bedb6b
Dima Skibin <sixtynice77@gmail.com>
2019-09-04 20:54:56 +0300

    Make base markup

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
-import Header from '../Header/Header';
 import './Skill.css';
 import Tree from '../Tree/Tree';
 
 export default function Skill() {
     return (
-        <div className="skill">
-            <Tree/>
-        </div>
+        <section className="skill">
+             <Tree />
+            <div className="skill-container">
+                <div className="skill-list-wrapper">
+                    <h2 className="skill__title">
+                        My Skills
+                </h2>
+                <ul className="skill__list">
+                    <li className="skill__item">HTML AND CSS</li>
+                    <li className="skill__item">JavaScript (ES6)</li>
+                    <li className="skill__item">React (in progress 😄)</li>
+                    <li className="skill__item">Git</li>
+                </ul>
+                </div>
+            </div>
+        </section>
     )
 }
